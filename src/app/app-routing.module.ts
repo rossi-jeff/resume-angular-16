e1adbbf14393d9f4cc3a5e17a8529705a9c8a619
Jeff Rossi <jrossi@intouchgps.com>
2023-05-08 10:04:54 -0400

    initial nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EducationComponent } from './education/education.component';
+import { EmploymentComponent } from './employment/employment.component';
+import { ContactComponent } from './contact/contact.component';
+import { ReferencesComponent } from './references/references.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ResumeComponent } from './resume/resume.component';
+import { LinksComponent } from './links/links.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'employment', component: EmploymentComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'references', component: ReferencesComponent },
+  { path: 'tesimonials', component: TestimonialsComponent },
+  { path: 'resume', component: ResumeComponent },
+  { path: 'links', component: LinksComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
